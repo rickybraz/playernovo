f60706c52eee7939f664f96ac414dd80f8cf6bcc
rickybraz <rickybraz@gmail.com>
2020-05-07 21:24:19 -0300

    Estrutura inicial do player responsivel

---
+++ _js/chamadas.js
@@ -0,0 +1,21 @@
+function busca(tecla){
+    if (tecla.keyCode ==13) {
+        window.alert("busca ainda não implantado")
+    }
+    
+}
+
+function BuscaEstilo() {
+    window.alert("oi")
+    var janela = document.getElementById("Janela")
+    janela.innerHTML = `<iframe src="_paginas/estilos.html">`
+    
+}
+
+function BuscaArtista() {
+    window.alert("oi")
+}
+
+function BuscaAlbuns() {
+    window.alert("oi")
+}
